a30f80e003c780ca63c8bca6d512761108f624a6
wenqingyu <yuwenqingisu@gmail.com>
2016-07-29 16:35:40 +0800

    42 tech

--- wp-login.php
+++ wp-login.php
@@ -9,14 +9,14 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>42 Tech &lsaquo; Log In</title>
 	<link rel='dns-prefetch' href='//s.w.org'>
-<link rel='stylesheet' href='http://wenqingyu.github.io/btcc-42tech/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6-RC1-38170' type='text/css' media='all' />
+<link rel='stylesheet' href='https://wenqingyu.github.io/btcc-42tech/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6-RC1-38170' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">42 Tech</a></h1>
 	
-<form name="loginform" id="loginform" action="http://wenqingyu.github.io/btcc-42tech/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://wenqingyu.github.io/btcc-42tech/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -28,13 +28,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="http://wenqingyu.github.io/btcc-42tech/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://wenqingyu.github.io/btcc-42tech/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="http://wenqingyu.github.io/btcc-42tech/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="https://wenqingyu.github.io/btcc-42tech/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -51,7 +51,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://wenqingyu.github.io/btcc-42tech/">&larr; Back to 42 Tech</a></p>
+	<p id="backtoblog"><a href="https://wenqingyu.github.io/btcc-42tech/">&larr; Back to 42 Tech</a></p>
 	
 	</div>
 
